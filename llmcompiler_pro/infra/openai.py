6925dba302f2cdc699eebb8df9d39f540c68501e
name1023 <relilau00@gmail.com>
2024-07-18 11:19:28 +0900

    Fix promptlayer accepts dynamically and update readme

--- llmcompiler_pro/infra/openai.py
+++ llmcompiler_pro/infra/openai.py
@@ -1,6 +1,12 @@
+import os
+
 import openai
+from openai import AsyncOpenAI
 from promptlayer import PromptLayer
 
 
 def factory_openai_async_client() -> openai.AsyncOpenAI:
-    return PromptLayer().openai.AsyncOpenAI()
+    if os.getenv("PROMPTLAYER_API_KEY") is None:
+        return AsyncOpenAI()
+    else:
+        return PromptLayer().openai.AsyncOpenAI()
