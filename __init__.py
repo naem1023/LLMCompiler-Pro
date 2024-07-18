82aa6be1d5cb68806384d9bc1726ed285cc3c357
name1023 <relilau00@gmail.com>
2024-07-18 11:25:53 +0900

    Remove init

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from langchain.tools import BaseTool
-
-from llmcompiler_pro.tools.browse.code_interpreter import CodeInterpreterTool
-from llmcompiler_pro.tools.browse.searching_and_browsing import SearchAndBrowsingTool
-
-
-def get_tools() -> list[BaseTool]:
-    """Get tools."""
-    return [SearchAndBrowsingTool(), CodeInterpreterTool()]
